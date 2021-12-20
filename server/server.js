0f4eda1e66550919b0f65f139dfa78d2a7326130
Hardik Mehta <info@tallyecom.in>
2021-12-20 16:22:13 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -64,7 +64,7 @@ server.use(function (req, res, next) {
   // );
   res.setHeader(
     "Content-Security-Policy",
-    `frame-ancestors ${shopurl} https://cambridgetestshop.myshopify.com https://admin.shopify.com https://*.myshopify.com https://example.myshopify.com`
+    `frame-ancestors ${shopurl} https://admin.shopify.com`
   );
   res.setHeader("Access-Control-Allow-Origin", "https://www.youtube.com/*");
   next();
