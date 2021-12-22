40b79861fa25eafd015a6b55bffeeabbab0a5702
Hardik Mehta <info@tallyecom.in>
2021-12-22 10:24:05 +0530

    moved csp to script

--- server/server.js
+++ server/server.js
@@ -53,24 +53,25 @@ server.get("/_next/*", (req, res) => {
 });
 
 //Content Security Policy
-server.use(function (req, res, next) {
-  var shopurl;
-  var fa;
-  console.log(req.query.shop !== "");
-  if (req.query.shop !== "") {
-    shopurl = "https://" + req.query.shop;
-    fa = `frame-ancestors ${shopurl} https://admin.shopify.com`;
-    console.log(shopurl);
-    console.log(fa);
-    res.setHeader(
-      "Content-Security-Policy-Report-Only",
-      fa
-      // `frame-ancestors ${shopurl} https://admin.shopify.com`
-    );
-    res.setHeader("Access-Control-Allow-Origin", "https://www.youtube.com/*");
-  }
-  next();
-});
+// server.use(function (req, res, next) {
+//   var shopurl;
+//   var fa;
+//   console.log(req.query.shop !== "");
+//   if (req.query.shop !== "") {
+//     shopurl = "https://" + req.query.shop;
+//     fa = `frame-ancestors ${shopurl} https://admin.shopify.com`;
+//     console.log(shopurl);
+//     console.log(fa);
+//     res.setHeader(
+//       "Content-Security-Policy-Report-Only",
+//       fa
+//       // `frame-ancestors ${shopurl} https://admin.shopify.com`
+//     );
+//     res.setHeader("Access-Control-Allow-Origin", "https://www.youtube.com/*");
+//   }
+//   next();
+// });
+
 //Authentication Url
 server.use("/auth", authRoutes);
 
