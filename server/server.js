a61af4bedcebfec5129f02ffd3f03a756e70b1d4
Hardik Mehta <info@tallyecom.in>
2021-12-23 20:30:36 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -59,7 +59,7 @@ server.use(function (req, res, next) {
   console.log(req.query.shop !== "");
   if (req.query.shop !== "") {
     shopurl = req.query.shop;
-    fa = `frame-ancestors ${shopurl}; admin.shopify.com;`;
+    fa = `frame-ancestors ${shopurl} admin.shopify.com`;
     console.log(shopurl);
     console.log(fa);
     res.setHeader(
