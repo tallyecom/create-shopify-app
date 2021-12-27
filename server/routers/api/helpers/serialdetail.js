8a78c72644f3aab2687fbd7619d29d2fc0169de0
Hardik Mehta <info@tallyecom.in>
2021-12-27 19:20:12 +0530

    Update serialdetail.js

--- server/routers/api/helpers/serialdetail.js
+++ server/routers/api/helpers/serialdetail.js
@@ -4,9 +4,13 @@ const Shop = require("../../../models/shop");
 const serialDetail = async (shop) => {
   try {
     let doc = await Shop.findOne(shop);
+    console.log(doc.tallyPrime);
     return doc;
   } catch (error) {
-    console.log("Error while fetching Registered serial from Database: ", error);
+    console.log(
+      "Error while fetching Registered serial from Database: ",
+      error
+    );
     return false;
   }
 };
